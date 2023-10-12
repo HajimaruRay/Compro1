aa92a22309a300bf59c50083c9cd115081497a38
HAJIMARU <144349636+HajimaruRay@users.noreply.github.com>
2023-10-12 13:22:42 +0700

    add 2 Q5

---
+++ Quiz/Q5/2_array_updown.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+void main()
+{
+    int k;
+    int sum1 = 0,sum2 = 0;
+    scanf("%d",&k);
+    int arr[k][k];
+
+    for (int i = 0;i < k;i++)
+    {
+        for (int j = 0;j < k;j++)
+        {
+            scanf("%d",&arr[i][j]);
+        }
+    }
+
+    for (int i = 0;i < k;i++)
+    {
+        for (int j = 0;j < k;j++)
+        {
+            if (j >= i+1)
+            {
+                sum1 += arr[i][j];
+            }
+            else if (j <= i-1)
+            {
+                sum2 += arr[i][j];
+            }
+        }
+    }
+
+    printf("%d\n%d",sum1,sum2);
+}
