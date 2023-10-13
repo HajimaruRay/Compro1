e693a416fd127247ae8c49ef41421c7cd29dd518
HajimaruRay <144349636+HajimaruRay@users.noreply.github.com>
2023-10-13 20:17:11 +0700

    Update 6_WaypointOrder.c

--- Quiz/Q5/6_WaypointOrder.c
+++ Quiz/Q5/6_WaypointOrder.c
@@ -3,6 +3,7 @@
 void main()
 {
     int rol,col;
+    
     scanf("%d%d",&rol,&col);
     int arr[rol+1][col+1];
 
@@ -16,8 +17,19 @@ void main()
 
     int k;
     scanf("%d",&k);
-    for (int i = 0;i < k;i++)
+    int Prol[k],Pcol[k];
+    for (int i = 1;i <= k;i++)
     {
-        
+        scanf("%d%d",&Prol[i],&Pcol[i]);
+        arr[Prol[i]][Pcol[i]] = i;
+    }
+
+    for (int i = 1;i <= rol;i++)
+    {
+        for (int j = 1;j <= col;j++)
+        {
+            printf("%d ",arr[i][j]);
+        }
+        printf("\n");
     }
 }
